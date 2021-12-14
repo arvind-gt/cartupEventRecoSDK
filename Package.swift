881b983a04ca89c0bb7424cf1f1dcfbf71834a58
arvind-gt <arvindrapaka@gmail.com>
2021-12-13 21:34:13 -0800

    Adding .

--- Package.swift
+++ Package.swift
@@ -2,10 +2,4 @@
 import PackageDescription
 let package = Package(
     name: "cartupEventRecoSDK",
-    products: [
-        .library(name: "cartupEventRecoSDK", targets: ["cartupEventRecoSDK"])
-    ],
-    targets: [
-        .target(name: "cartupEventRecoSDK", path: "source")
-    ]
 )
